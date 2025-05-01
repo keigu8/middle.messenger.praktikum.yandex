b61b581d5711ddc89565c72bba95860f9698620b
keigu <kate.gur.ova@yandex.ru>
2025-05-01 13:09:49 +0300

    Project setup

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import handlebars from "vite-plugin-handlebars";
+
+export default defineConfig({
+  root: resolve(__dirname, "src"),
+  build: {
+    outDir: resolve(__dirname, "dist"),
+  },
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, "src/partials"),
+      context: {},
+    }),
+  ],
+});
