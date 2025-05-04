6c1d2e903d7c4712d6a35938e11267d46465933c
keigu <kate.gur.ova@yandex.ru>
2025-05-04 19:35:49 +0300

    Добавлена страница профиля

--- src/context.js
+++ src/context.js
@@ -1,9 +1,11 @@
 import { login } from "./pages/login";
+import { profile } from "./pages/profile/profile";
 import { signup } from "./pages/signup";
 
 export default {
   pages: {
     login,
     signup,
+    profile,
   },
 };
---
+++ src/pages/profile/profile.js
@@ -0,0 +1,36 @@
+export const profile = {
+  title: "Профиль",
+  avatar: {
+    src: "../../assets/avatar.png",
+    alt: "Аватар профиля",
+  },
+  data: [
+    {
+      label: "Почта",
+      value: "pochta@yandex.ru",
+    },
+    {
+      label: "Логин",
+      value: "ivanivanov",
+    },
+    {
+      label: "Имя",
+      value: "Иван",
+    },
+    {
+      label: "Фамилия",
+      value: "Иванов",
+    },
+    {
+      label: "Имя в чате",
+      value: "Иван",
+    },
+    {
+      label: "Телефон",
+      value: "+7 (909) 967 30 30",
+    },
+  ],
+  editProfileButtonTitle: "Редактировать",
+  editPasswordButtonTitle: "Изменить пароль",
+  logoutButtonTitle: "Выйти",
+};
