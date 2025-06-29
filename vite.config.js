8541d37420a4cdbbe5e75e9bcecc5fa19ca48699
keigu <kate.gur.ova@yandex.ru>
2025-06-29 21:29:43 +0300

    Разрешен top level await

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,20 @@ import { defineConfig, loadEnv } from "vite";
 import { resolve } from "path";
 
 export default ({ mode }) => {
-  
   // eslint-disable-next-line no-undef
-  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
-  
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
   return defineConfig({
-  // eslint-disable-next-line
-  root: resolve(__dirname, "src"),
-  build: {
+    esbuild: {
+      supported: {
+        "top-level-await": true, //browsers can handle top-level-await features
+      },
+    },
     // eslint-disable-next-line
+  root: resolve(__dirname, "src"),
+    build: {
+      // eslint-disable-next-line
     outDir: resolve(__dirname, "dist"),
-  },
-});
-}
+    },
+  });
+};
